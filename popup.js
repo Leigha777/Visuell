0ce317ab7576259d13bd39763165ce6845fb8ccf
Andrew <achow101@gmail.com>
2016-05-21 16:50:42 -0400

    Calculates zoom factor based on visual acuity
    Calculates and zooms based on entered visual acuity and distance from
    screen.

--- popup.js
+++ popup.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById('status').textContent = "Testing";
     var zoomButton = document.getElementById('changeZoomButton');
     zoomButton.addEventListener('click', changezoom);
 });
 
 function changezoom()
 {
-  console.log("Clicked!!!!!");
+  var distance = parseInt(document.getElementById("dist").value);
+  var visAc = parseInt(document.getElementById("visAc").value);
+  var neededDist = 20*distance/visAc;
+  var zoomFactor = distance/neededDist;
   chrome.tabs.getSelected(null, function(tab)
   {
     var tabId = tab.id;
-    chrome.tabs.setZoom(tabId, 0.55, null);
+    chrome.tabs.setZoom(tabId, zoomFactor, null);
   })
 }
