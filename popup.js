106429a337f2af764d436f14cee5c5954f395e01
Andrew <achow101@gmail.com>
2016-05-21 14:19:11 -0400

    Zoom Button
    Button which changes the zoom with a test value.

--- popup.js
+++ popup.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById('status').textContent = "Testing";
+    var zoomButton = document.getElementById('changeZoomButton');
+    zoomButton.addEventListener('click', changezoom);
 });
+
+function changezoom()
+{
+  console.log("Clicked!!!!!");
+  chrome.tabs.getSelected(null, function(tab)
+  {
+    var tabId = tab.id;
+    chrome.tabs.setZoom(tabId, 0.55, null);
+  })
+}
