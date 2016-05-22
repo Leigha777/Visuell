c5cf45e5d20f95c2f28bfd5a1d75e69c5db87e17
Andrew <achow101@gmail.com>
2016-05-21 23:10:45 -0400

    Dioptric Option

--- popup.js
+++ popup.js
@@ -1,36 +1,39 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    var zoomButton = document.getElementById('changeZoomButton');
-    zoomButton.addEventListener('click', changezoomclick);
-    var metric = false;
-    var metricChk = document.getElementById("metricChk");
-    metricChk.addEventListener('change', toggleMetric);
-    restore_zoom();
+var metric = false;
 
-    document.getElementById('snellen20200').addEventListener('click', function() {
-      changezoom(2, 200)
-    });
-    document.getElementById('snellen20100').addEventListener('click', function() {
-      changezoom(2, 100)
-    });
-    document.getElementById('snellen2070').addEventListener('click', function() {
-      changezoom(2, 70)
-    });
-    document.getElementById('snellen2050').addEventListener('click', function() {
-      changezoom(2, 50)
-    });
-    document.getElementById('snellen2040').addEventListener('click', function() {
-      changezoom(2, 40)
-    });
-    document.getElementById('snellen2030').addEventListener('click', function() {
-      changezoom(2, 30)
-    });
-    document.getElementById('snellen2025').addEventListener('click', function() {
-      changezoom(2, 25)
-    });;
-    document.getElementById('snellen2020').addEventListener('click', function() {
-      changezoom(2, 20)
-    });
+document.addEventListener('DOMContentLoaded', function() {
+
+  restore_zoom();
+
+  document.getElementById('changeZoomButton').addEventListener('click', changezoomclick);
+  document.getElementById("metricChk").addEventListener('change', toggleMetric);
+
+  document.getElementById('snellen20200').addEventListener('click', function() {
+    changezoom(2, 200)
+  });
+  document.getElementById('snellen20100').addEventListener('click', function() {
+    changezoom(2, 100)
+  });
+  document.getElementById('snellen2070').addEventListener('click', function() {
+    changezoom(2, 70)
+  });
+  document.getElementById('snellen2050').addEventListener('click', function() {
+    changezoom(2, 50)
+  });
+  document.getElementById('snellen2040').addEventListener('click', function() {
+    changezoom(2, 40)
+  });
+  document.getElementById('snellen2030').addEventListener('click', function() {
+    changezoom(2, 30)
+  });
+  document.getElementById('snellen2025').addEventListener('click', function() {
+    changezoom(2, 25)
+  });;
+  document.getElementById('snellen2020').addEventListener('click', function() {
+    changezoom(2, 20)
+  });
+
+  document.getElementById('dioptriczoombutton').addEventListener('click', dioptriczoomclick);
 });
 
 /*
@@ -47,14 +50,20 @@ function reloadZoom()
 
 function changezoomclick()
 {
-    var distance = parseFloat(document.getElementById("dist").value);
-    var visAc = parseFloat(document.getElementById("visAc").value);
-    changezoom(distance, visAc);
+  var distance = parseFloat(document.getElementById("dist").value);
+  var visAc = parseFloat(document.getElementById("visAc").value);
+  changezoom(distance, visAc);
 }
 
 function changezoom(distance, visAc)
 {
-  var neededDist = 20*distance/visAc;
+  var base = 20;
+  if(metric)
+  {
+    base = 6;
+  }
+
+  var neededDist = base*distance/visAc;
   var zoomFactor = distance/neededDist;
   chrome.tabs.getSelected(null, function(tab)
   {
@@ -102,4 +111,100 @@ function toggleMetric()
     scaleDiv.innerHTML = "20/<input id=\"visAc\"></input>";
   }
   restore_zoom();
+}
+
+function dioptriczoomclick()
+{
+  var dods = parseFloat(document.getElementById('dods').value);
+  var doss = parseFloat(document.getElementById('doss').value);
+  var aods = parseFloat(document.getElementById('aods').value);
+  var aoss = parseFloat(document.getElementById('aoss').value);
+  var ods = dods + aods;
+  var oss = doss + aoss;
+  var sph = (ods + oss)/2;
+
+  if(sph >= -0.5)
+  {
+    document.getElementById('opt1').addEventListener('click', function() {
+      changezoom(2, 20);
+    });
+    document.getElementById('opt2').addEventListener('click', function() {
+      changezoom(2, 30);
+    });
+    document.getElementById('opt3').addEventListener('click', function() {
+      changezoom(2, 40);
+    });
+  }
+  else if(sph >= -0.75)
+  {
+    document.getElementById('opt1').addEventListener('click', function() {
+      changezoom(2, 40);
+    });
+    document.getElementById('opt2').addEventListener('click', function() {
+      changezoom(2, 50);
+    });
+    document.getElementById('opt3').addEventListener('click', function() {
+      changezoom(2, 60);
+    });
+  }
+  else if(sph >= -1.0)
+  {
+    document.getElementById('opt1').addEventListener('click', function() {
+      changezoom(2, 50);
+    });
+    document.getElementById('opt2').addEventListener('click', function() {
+      changezoom(2, 60);
+    });
+    document.getElementById('opt3').addEventListener('click', function() {
+      changezoom(2, 70);
+    });
+  }
+  else if(sph >= -1.25)
+  {
+    document.getElementById('opt1').addEventListener('click', function() {
+      changezoom(2, 60);
+    });
+    document.getElementById('opt2').addEventListener('click', function() {
+      changezoom(2, 70);
+    });
+    document.getElementById('opt3').addEventListener('click', function() {
+      changezoom(2, 80);
+    });
+  }
+  else if(sph >= -1.50)
+  {
+    document.getElementById('opt1').addEventListener('click', function() {
+      changezoom(2, 80);
+    });
+    document.getElementById('opt2').addEventListener('click', function() {
+      changezoom(2, 90);
+    });
+    document.getElementById('opt3').addEventListener('click', function() {
+      changezoom(2, 100);
+    });
+  }
+  else if(sph >= -2.5)
+  {
+    document.getElementById('opt1').addEventListener('click', function() {
+      changezoom(2, 100);
+    });
+    document.getElementById('opt2').addEventListener('click', function() {
+      changezoom(2, 150);
+    });
+    document.getElementById('opt3').addEventListener('click', function() {
+      changezoom(2, 200);
+    });
+  }
+  else
+  {
+    document.getElementById('opt1').addEventListener('click', function() {
+      changezoom(2, 10);
+    });
+    document.getElementById('opt2').addEventListener('click', function() {
+      changezoom(2, 20);
+    });
+    document.getElementById('opt3').addEventListener('click', function() {
+      changezoom(2, 30);
+    });
+  }
 }
